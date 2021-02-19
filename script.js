f6f67785a74ef3bddb64668c5dbef76cd80e7ab9
danilosp1 <danilo.sp03@gmail.com>
2021-02-19 15:35:23 -0300

    Movimentação e correção de bugs

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+
 let snake = [];
 snake[0] = {
-  x:8 * box,
+  x: 8 * box,
   y: 8 * box
 }
 
+let direction = "right";
 
 function makeBG(){
   context.fillStyle = "lightgreen";
@@ -20,5 +22,58 @@ function makeSnake(){
   }
 }
 
-makeBG();
-makeSnake();
+document.addEventListener('keydown', update);
+
+function update(event){
+  if(event.keyCode == 37 && direction != "right"){
+    direction = "left";
+  }
+  if(event.keyCode == 38 && direction != "down"){
+    direction = "up";
+  }
+  if(event.keyCode == 39 && direction != "left"){
+    direction = "right";
+  }
+  if(event.keyCode == 40 && direction != "up"){
+    direction = "down";
+  }
+}
+
+function startGame(){
+  if(snake[0].x > 15 * box && (direction == "right" || direction == "up" || direction == "down")){
+    snake[0].x = 0;
+  }
+  if(snake[0].x < 0 && (direction == "left" || direction == "up" || direction == "down")){
+    snake[0].x = 16 * box;
+  }
+  if(snake[0].y > 15 * box && (direction == "down" || direction == "right" || direction == "left")){
+    snake[0].y = 0;
+  }
+  if(snake[0].y < 0 && (direction == "up" || direction == "right" || direction == "left")){
+    snake[0].y = 16 * box;
+  }
+
+  makeBG();
+  makeSnake();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  switch (direction){
+    case "right": snakeX += box; break;
+    case "left": snakeX -= box; break;
+    case "up": snakeY -= box; break;
+    case "down": snakeY += box; break;
+  }
+
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  }
+
+  snake.unshift(newHead);
+}
+
+let jogo = setInterval(startGame, 100);
